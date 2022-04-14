cfe14fd45f07973b94c45b0d759704dcb210ecc6
Retr0327 <philcoke35@gmail.com>
2022-04-14 20:54:15 +0800

    Add koaBody

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,11 +1,13 @@
 import Koa from "koa";
 import cors from "@koa/cors";
+import koaBody from "koa-body";
 import corsConfig from "./helpers/corsConfig";
 import { uploadGloss } from "./routes/uploadGloss";
 
 const app = new Koa();
 
 app.use(cors(corsConfig));
+app.use(koaBody({ urlencoded: true, multipart: true }));
 
 app.use(uploadGloss.routes());
 app.use(uploadGloss.allowedMethods());
