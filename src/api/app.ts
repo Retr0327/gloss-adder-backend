bb503034e908e80f56e9c17dc40b6a0b4b4df305
Retr0327 <philcoke35@gmail.com>
2022-04-14 23:10:06 +0800

    Setup upload directory

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,8 +1,14 @@
+import fs from "fs";
 import Koa from "koa";
 import cors from "@koa/cors";
 import koaBody from "koa-body";
 import corsConfig from "./helpers/corsConfig";
+import { UPLOAD_DIR } from "./constants/constants";
 import { uploadGloss } from "./routes/uploadGloss";
+
+if (!fs.existsSync(UPLOAD_DIR)) {
+  fs.mkdirSync(UPLOAD_DIR);
+}
 
 const app = new Koa();
 
