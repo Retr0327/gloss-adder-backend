cf6640c73ee3619d01a9ef57d3b858808f913ad3
Retr0327 <philcoke35@gmail.com>
2022-04-14 15:44:40 +0800

    Add cors

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,6 +1,10 @@
 import Koa from "koa";
+import cors from "@koa/cors";
+import corsConfig from "./helpers/corsConfig";
 
 const app = new Koa();
+
+app.use(cors(corsConfig));
 
 app.use(async (ctx) => {
   ctx.body = "hello world";
