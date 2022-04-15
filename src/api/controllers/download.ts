2e4c9bf6d87cc64157cff68c2092b5ebc64f66c5
Retr0327 <philcoke35@gmail.com>
2022-04-15 21:11:07 +0800

    Update handleDownload

--- src/api/controllers/download.ts
+++ src/api/controllers/download.ts
@@ -1,9 +1,15 @@
 import fs from "fs";
 import path from "path";
 import { Context } from "koa";
+import { getUploadedFileDir } from "../constants";
 
 const handleDownload = async (ctx: Context) => {
-  console.log(ctx.request.query);
+  const { token } = ctx.params;
+  const uploadedFileDir = getUploadedFileDir(token);
+  const allFiles = fs.readdirSync(uploadedFileDir);
+
+  console.log(allFiles)
+
 };
 
 export default handleDownload;
