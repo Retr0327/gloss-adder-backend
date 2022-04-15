87da1e6e94d87851794240bdd2b8db43f351f026
Retr0327 <philcoke35@gmail.com>
2022-04-15 23:35:36 +0800

    Update download

--- src/api/controllers/download.ts
+++ src/api/controllers/download.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { Context } from "koa";
+import zipFiles from "../helpers/zipFiles";
 import { getUploadedFileDir } from "../constants";
 
 const handleDownload = async (ctx: Context) => {
@@ -8,8 +9,19 @@ const handleDownload = async (ctx: Context) => {
   const uploadedFileDir = getUploadedFileDir(token);
   const allFiles = fs.readdirSync(uploadedFileDir);
 
-  console.log(allFiles)
+  if (allFiles.length > 1) {
+    await zipFiles(token);
+    const zipFile = path.join(uploadedFileDir, `${token}.zip`);
+    ctx.attachment(zipFile);
+    ctx.status = 200;
+    ctx.body = fs.createReadStream(zipFile);
+    return;
+  }
 
+  const downloadFile = path.join(uploadedFileDir, allFiles[0]);
+  ctx.set("Content-disposition", `attachment; filename=${downloadFile}`);
+  ctx.status = 200;
+  ctx.body = fs.createReadStream(downloadFile);
 };
 
 export default handleDownload;
