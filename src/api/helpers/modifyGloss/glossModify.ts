963910ddb373f6fa460a50284b1f27c9a44bc4a3
Retr0327 <philcoke35@gmail.com>
2022-04-15 20:44:35 +0800

    Update glossModify

--- src/api/helpers/modifyGloss/glossModify.ts
+++ src/api/helpers/modifyGloss/glossModify.ts
@@ -1,10 +1,18 @@
 import fs from "fs";
+import { writeGloss, GlossAdder } from "./utils";
 import { getUploadedFileDir } from "../../constants";
 
 async function glossModify(token: string, cliticOption: string) {
   const uploadedFileDir = getUploadedFileDir(token);
 
-  console.log(uploadedFileDir);
+  fs.readdir(uploadedFileDir, async (err, files) => {
+    for (let file of files) {
+      const fileName = `${uploadedFileDir}/${file}`;
+      const gloss = new GlossAdder(fileName, "utf8", parseInt(cliticOption));
+      const glossContent = await gloss.add();
+      await writeGloss(fileName, glossContent, "utf8");
+    }
+  });
 }
 
 export default glossModify;
