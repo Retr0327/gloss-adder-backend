659ab737c9c1bf866fd967a37c0410e1d15fa30f
Retr0327 <philcoke35@gmail.com>
2022-04-15 11:45:36 +0800

    Update createUploadFiles

--- src/api/helpers/uploadFile/createUploadFiles.ts
+++ src/api/helpers/uploadFile/createUploadFiles.ts
@@ -1,12 +1,27 @@
 import fs from "fs";
-import { getUploadFileDir } from "src/api/constants";
+import { getUploadFileDir } from "../../constants";
 
 type bufferResultType = [field: Buffer, value: Buffer][];
 
 async function createUploadFiles(
-  token: number,
+  token: string,
   bufferResult: bufferResultType
 ) {
+  const uploadLocation = getUploadFileDir(token);
+
+  fs.mkdir(uploadLocation, { recursive: true }, (error) => {
+    if (error) {
+      console.log("cannot upload to a new directory", error);
+    }
+  });
+
+  const arrayOfFiles = Object.entries(bufferResult).map((value) => {
+    const [timeStringName, buffer] = value;
+    const fileName = timeStringName.match(/(?<=\d\-\d\-).*/g)![0];
+
+    return { fileName, data: buffer };
+  });
+
   return null;
 }
 
