dd7d114093184f11b111c4f4ea0c2a769f772168
Retr0327 <philcoke35@gmail.com>
2022-04-15 21:16:03 +0800

    Update zipFiles

--- src/api/helpers/zipFiles.ts
+++ src/api/helpers/zipFiles.ts
@@ -1,7 +1,26 @@
 import fs from "fs";
 import path from "path";
 import archiver from "archiver";
+import { getUploadedFileDir } from "../constants";
 
-async function zipFiles(token: string) {}
+async function zipFiles(token: string) {
+  const uploadLocation = getUploadedFileDir(token);
+  const zipPath = path.join(uploadLocation, token) + ".zip";
+  const output = fs.createWriteStream(zipPath);
+  const zipArchiver = archiver("zip", { zlib: { level: 9 } });
+
+  return new Promise<void>((resolve, reject) => {
+    zipArchiver.on("error", (err) => reject(err));
+    zipArchiver.pipe(output);
+
+    zipArchiver.glob("**", {
+      cwd: uploadLocation,
+      ignore: [".DS_Store", "*.zip"],
+    });
+
+    output.on("close", () => resolve());
+    zipArchiver.finalize();
+  });
+}
 
 export default zipFiles;
