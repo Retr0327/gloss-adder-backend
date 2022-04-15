b8e6edbe59b849869448b43635464fb8b07c4f39
Retr0327 <philcoke35@gmail.com>
2022-04-15 20:25:13 +0800

    Add glossReader

---
+++ src/api/helpers/modifyGloss/utils/glossReader.ts
@@ -0,0 +1,48 @@
+import { promises as fs } from "fs";
+
+class GlossReader {
+  fileName: string;
+  encoding: BufferEncoding;
+
+  constructor(fileName: string, encoding: BufferEncoding = "utf8") {
+    this.fileName = fileName;
+    this.encoding = encoding;
+  }
+
+  renumber(filePath: string[]) {
+    let count = 0;
+    for (const [index, element] of filePath.entries()) {
+      const isNumber = /^\d\./;
+      if (isNumber.test(element)) {
+        filePath[index] = `${count + 1}.`;
+        count++;
+      }
+    }
+    return filePath;
+  }
+
+  readLines(file: string | undefined) {
+    if (!file) {
+      throw new Error("cannot readLines");
+    }
+    const splitedFile = file.split("\n");
+    return splitedFile.map((value: string) => value.trim());
+  }
+
+  async processFile(filePath: string) {
+    try {
+      const output = await fs.readFile(filePath, { encoding: this.encoding });
+      return output.toString();
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  }
+
+  async read() {
+    const file = await this.processFile(this.fileName);
+    const fileArray = this.readLines(file);
+    return this.renumber(fileArray);
+  }
+}
+
+export default GlossReader;
