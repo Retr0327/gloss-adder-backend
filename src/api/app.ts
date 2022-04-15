099e8473d0bf78ec60d6b56beb6eecf2ced1d362
Retr0327 <philcoke35@gmail.com>
2022-04-15 21:06:15 +0800

    Import download route

--- src/api/app.ts
+++ src/api/app.ts
@@ -4,7 +4,7 @@ import cors from "@koa/cors";
 import koaBody from "koa-body";
 import { UPLOAD_DIR } from "./constants";
 import corsConfig from "./helpers/corsConfig";
-import { rootRoute, uploadGloss } from "./routes";
+import { rootRoute, uploadGloss, download } from "./routes";
 
 if (!fs.existsSync(UPLOAD_DIR)) {
   fs.mkdirSync(UPLOAD_DIR);
@@ -19,5 +19,7 @@ app.use(uploadGloss.routes());
 app.use(uploadGloss.allowedMethods());
 app.use(rootRoute.routes());
 app.use(rootRoute.allowedMethods());
+app.use(download.routes());
+app.use(download.allowedMethods());
 
 export default app;
