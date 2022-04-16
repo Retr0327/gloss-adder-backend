4944ef817674f4b5777d2b956c42cd9f8736b2ca
Retr0327 <philcoke35@gmail.com>
2022-04-16 14:21:32 +0800

    Update handleDownload

--- src/api/controllers/download.ts
+++ src/api/controllers/download.ts
@@ -3,25 +3,35 @@ import path from "path";
 import { Context } from "koa";
 import zipFiles from "../helpers/zipFiles";
 import { getUploadedFileDir } from "../constants";
+import removeUploadedFolder from "../helpers/removeUploadFolder";
 
 const handleDownload = async (ctx: Context) => {
   const { token } = ctx.params;
-  const uploadedFileDir = getUploadedFileDir(token);
-  const allFiles = fs.readdirSync(uploadedFileDir);
 
-  if (allFiles.length > 1) {
-    await zipFiles(token);
-    const zipFile = path.join(uploadedFileDir, `${token}.zip`);
-    ctx.attachment(zipFile);
+  try {
+    const uploadedFileDir = getUploadedFileDir(token);
+    const allFiles = fs.readdirSync(uploadedFileDir);
+
+    if (allFiles.length > 1) {
+      await zipFiles(token);
+      const zipFile = path.join(uploadedFileDir, `${token}.zip`);
+      ctx.attachment(zipFile);
+      ctx.status = 200;
+      ctx.body = fs.createReadStream(zipFile);
+      return;
+    }
+
+    const downloadFile = path.join(uploadedFileDir, allFiles[0]);
+    ctx.attachment(downloadFile);
     ctx.status = 200;
-    ctx.body = fs.createReadStream(zipFile);
-    return;
+    ctx.body = fs.createReadStream(downloadFile);
+  } catch (error) {
+    ctx.staus = 400;
+    ``;
+    ctx.body = { status: "failed" };
+  } finally {
+    removeUploadedFolder(token);
   }
-
-  const downloadFile = path.join(uploadedFileDir, allFiles[0]);
-  ctx.set("Content-disposition", `attachment; filename=${downloadFile}`);
-  ctx.status = 200;
-  ctx.body = fs.createReadStream(downloadFile);
 };
 
 export default handleDownload;
