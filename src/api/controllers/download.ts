9c03972a253b9ff9ec65f66279ef64a21031b85e
Retr0327 <philcoke35@gmail.com>
2022-04-16 14:36:22 +0800

    Update handleDownload

--- src/api/controllers/download.ts
+++ src/api/controllers/download.ts
@@ -1,32 +1,21 @@
 import fs from "fs";
-import path from "path";
+import {
+  removeUploadedFolder,
+  downloadFileController,
+} from "../helpers/download";
 import { Context } from "koa";
-import { getUploadedFileDir } from "../constants";
-import { zipFiles, removeUploadedFolder } from "../helpers/download";
 
 const handleDownload = async (ctx: Context) => {
   const { token } = ctx.params;
 
   try {
-    const uploadedFileDir = getUploadedFileDir(token);
-    const allFiles = fs.readdirSync(uploadedFileDir);
+    const downloadFile = await downloadFileController(token);
 
-    if (allFiles.length > 1) {
-      await zipFiles(token);
-      const zipFile = path.join(uploadedFileDir, `${token}.zip`);
-      ctx.attachment(zipFile);
-      ctx.status = 200;
-      ctx.body = fs.createReadStream(zipFile);
-      return;
-    }
-
-    const downloadFile = path.join(uploadedFileDir, allFiles[0]);
     ctx.attachment(downloadFile);
     ctx.status = 200;
     ctx.body = fs.createReadStream(downloadFile);
   } catch (error) {
     ctx.staus = 400;
-    ``;
     ctx.body = { status: "failed" };
   } finally {
     removeUploadedFolder(token);
