2419c3f2fe99189ff5346a0d4f159698cea053de
Retr0327 <philcoke35@gmail.com>
2022-04-16 15:49:42 +0800

    Update constatns

--- src/api/constants/index.ts
+++ src/api/constants/index.ts
@@ -3,4 +3,10 @@ import path from "path";
 const UPLOAD_DIR: string = path.resolve("src", "api", "upload");
 const getUploadedFileDir = (name: string) => path.resolve(UPLOAD_DIR, name);
 
-export { UPLOAD_DIR, getUploadedFileDir };
+const dockerEnv = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  koaPort: process.env.KOA_PORT,
+};
+
+export { UPLOAD_DIR, getUploadedFileDir, dockerEnv };
