6e04708b1cda7157e1a986d097f78a29f68d0d37
Retr0327 <philcoke35@gmail.com>
2022-04-18 03:46:28 +0800

    Set port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import app from "./api/app";
+import { dockerEnv } from "./api/constants";
 
-const PORT = process.env.PORT || 3000;
+const { port } = dockerEnv;
+
+const PORT = port || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
