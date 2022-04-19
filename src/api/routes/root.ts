7de0c9f430fac5c991a1c1e38cd4f8e57248179b
Retr0327 <philcoke35@gmail.com>
2022-04-19 14:03:29 +0800

    Update response body

--- src/api/routes/root.ts
+++ src/api/routes/root.ts
@@ -5,7 +5,7 @@ const router = new KoaRouter();
 router.get("/", (ctx) => {
   const ip = ctx.request.ip;
   ctx.status = 200;
-  ctx.body = { message: "success", ip };
+  ctx.body = { status: "success", ip };
 });
 
 export { router as rootRoute };
