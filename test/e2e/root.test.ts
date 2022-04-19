7cd7121995efd8ee32c070a899d1fded5824b502
Retr0327 <philcoke35@gmail.com>
2022-04-19 14:17:18 +0800

    Remove console.log

--- test/e2e/root.test.ts
+++ test/e2e/root.test.ts
@@ -3,10 +3,7 @@ import app from "../../src/server";
 
 describe("Test root /", () => {
   test("should return success and ip", async () => {
-    console.log("hello");
     const response = await request(app).get("/");
-
-    console.log(response.body);
     expect(response.statusCode).toEqual(200);
     expect(response.body).toHaveProperty("status", "success");
     expect(response.body).toHaveProperty("ip", "::ffff:127.0.0.1");
