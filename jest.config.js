b0c67ba3d401ecdb7935c6b57ef94452de743141
Retr0327 <philcoke35@gmail.com>
2022-04-19 19:41:56 +0800

    Add jest config

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  clearMocks: true,
+
+  coverageDirectory: "coverage",
+
+  coveragePathIgnorePatterns: ["<rootDir>/node_modules/(?!@foo)"],
+
+  moduleFileExtensions: ["ts", "js"],
+
+  moduleNameMapper: {
+    "src/(.*)": "<rootDir>/src/$1",
+  },
+
+  testEnvironment: "node",
+
+  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+
+  transformIgnorePatterns: ["<rootDir>/node_modules/(?!@foo)"],
+};
