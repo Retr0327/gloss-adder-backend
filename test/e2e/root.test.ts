90108f47485c8494f6297faa2f40e3969edd493a
Retr0327 <philcoke35@gmail.com>
2022-04-19 14:02:59 +0800

    Update root.test

--- test/e2e/root.test.ts
+++ test/e2e/root.test.ts
@@ -1,4 +1,14 @@
-import app from "../../src/api/app";
-import request from 'supertest'
+import request from "supertest";
+import app from "../../src/server";
 
-test()
+describe("Test root /", () => {
+  test("should return success and ip", async () => {
+    console.log("hello");
+    const response = await request(app).get("/");
+
+    console.log(response.body);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty("status", "success");
+    expect(response.body).toHaveProperty("ip", "::ffff:127.0.0.1");
+  });
+});
