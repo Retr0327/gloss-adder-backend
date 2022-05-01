a261f44bfa557f2843e16efa27e78a4bb671a178
Retr0327 <philcoke35@gmail.com>
2022-05-01 19:36:10 +0800

    Remove dockerEnv

--- src/api/constants/index.ts
+++ src/api/constants/index.ts
@@ -3,11 +3,4 @@ import path from "path";
 const UPLOAD_DIR: string = path.resolve("src", "api", "upload");
 const getUploadedFileDir = (name: string) => path.resolve(UPLOAD_DIR, name);
 
-const dockerEnv = {
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
-  port: process.env.PORT,
-  period: process.env.NODE_ENV,
-};
-
-export { UPLOAD_DIR, getUploadedFileDir, dockerEnv };
+export { UPLOAD_DIR, getUploadedFileDir };
