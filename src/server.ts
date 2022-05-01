8f810377c4af1b80bf297ea7b94d262c1ee2b21f
Retr0327 <philcoke35@gmail.com>
2022-05-01 19:36:50 +0800

    Update server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import app from "./api/app";
-import { dockerEnv } from "./api/constants";
 
-const { port } = dockerEnv;
-
-const PORT = port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
